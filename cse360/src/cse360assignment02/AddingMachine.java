4164d8be130bf66fb709d171fa8949b40012dc57
lkdonne1 <lkdonne1@asu.edu>
2020-10-07 22:37:51 -0700

    Filled out the functions in AddingMachine.java

--- cse360/src/cse360assignment02/AddingMachine.java
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -3,25 +3,33 @@ package cse360assignment02;
 /* Adding Machine Class */
 public class AddingMachine {
   private int total;
+  private String history;
   /* Constructor*/
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "" + total;
   }
-   /* Returns total */
+   /* Returns current total */
   public int getTotal () {
-    return 0;
+    return this.total;
   }
-  /* adds the value */
+  /* adds the value to total and transaction to history */
   public void add (int value) {
+	  this.total += value;
+	  this.history = history + " + " + value;
   }
-  /* subtracts the value*/
+  /* subtracts the value from the total variable and adds transaction to history*/
   public void subtract (int value) {
+	  this.total -= value;
+	  this.history = history + " - " + value;
   }
   /* returns the string*/
   public String toString () {
     return "";
   }
-  /* clears the value */
+  /* clears the total and history */
   public void clear() {
+	  this.total =0;
+	  this.history = "0";
   }
 }
