d1fb8bf7d1e880e2cd6603fdebfb92c6a8428844
lkdonne1 <lkdonne1@asu.edu>
2020-10-07 21:45:47 -0700

    created class

---
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,27 @@
+package cse360assignment02;
+
+/* Adding Machine Class */
+public class AddingMachine {
+  private int total;
+  /* Constructor*/
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+   /* Returns total */
+  public int getTotal () {
+    return 0;
+  }
+  /* adds the value */
+  public void add (int value) {
+  }
+  /* subtracts the value*/
+  public void subtract (int value) {
+  }
+  /* returns the string*/
+  public String toString () {
+    return "";
+  }
+  /* clears the value */
+  public void clear() {
+  }
+}
